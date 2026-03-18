be4b7f00b407f97c7b369a0fc788cfa20b6d517e
truglodite <imelman@msn.com>
2026-03-18 14:43:56 -0700

    Revert esc init at top; moved back to just before IMU init.

--- firmware/src/main.go
+++ firmware/src/main.go
@@ -103,52 +103,7 @@ type flightState int
 
 // main is the entry point for the TinyGo program.
 func main() {
-	// configure the onboard RGB LED (Low=on, High=off)
-	redLED.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	greenLED.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	blueLED.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	// ESC init right away to avoid leaving some esc's in a bad state
-	// Reset retries for the next component
-	setLED(1) // R for esc init
-	var retries = 0
-escInit:
-	if USE_DSHOT {
-		escPin = PWM_CH3_PIN
-		escPin.Configure(machine.PinConfig{Mode: machine.PinOutput}) // error checking is apparently not needed for PinConfig
-		setESC(MIN_PULSE_WIDTH_US)
-		println("DShot configured for ESC.")
-	} else {
-		escPWMConfig := machine.PWMConfig{
-			Period: machine.GHz * 1 / ESC_PWM_FREQUENCY,
-		}
-		if err = pwm1.Configure(escPWMConfig); err != nil {
-			setLED(7) // RGB on esc init error
-			retries++
-			if retries < 5 {
-				time.Sleep(100 * time.Millisecond)
-				goto escInit
-			}
-			// Fallback or panic if max retries exceeded
-			println("CRITICAL: Servo PWM Channel 5 Init Failed")
-			return
-		}
-		escPeriodNs = escPWMConfig.Period
-		escCh, err = pwm1.Channel(PWM_CH3_PIN)
-		if err != nil {
-			setLED(7) // RGB on esc pwm init error
-			retries++
-			if retries < 5 {
-				time.Sleep(100 * time.Millisecond)
-				goto escInit
-			}
-			// Fallback or panic if max retries exceeded
-			println("CRITICAL: ESC PWM Init Failed")
-			return
-		}
-		// need to add way to calibrate pwm esc's
-		setESC(MIN_PULSE_WIDTH_US)
-		println("PWM configured for ESC.")
-	}
+
 	//time.Sleep(2 * time.Second) // Wait for hardware to stabilize
 	println("WingFC Flight Controller - Version", Version)
 	println("A TinyGo Flight Controller for Flying Wing Aircraft")
@@ -156,6 +111,11 @@ escInit:
 	println("Author: Bryan Souza (github.com/BryanSouza91)")
 
 	println("Initializing...")
+
+	// configure the onboard RGB LED (Low=on, High=off)
+	redLED.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	greenLED.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	blueLED.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
 	// --- Hardware Setup ---
 	uart.Configure(machine.UARTConfig{
@@ -167,7 +127,7 @@ escInit:
 
 	setLED(1) // G for servo config
 
-	retries = 0
+	var retries = 0
 servoPWMInit:
 	servoPWMConfig := machine.PWMConfig{
 		Period: machine.GHz * 1 / SERVO_PWM_FREQUENCY,
@@ -251,6 +211,49 @@ servoCh5Init:
 	// set servos 1, 2, 4, 5 (not yet 6) to subtrim values
 	setServo(NEUTRAL_RX_VALUE, NEUTRAL_RX_VALUE, NEUTRAL_RX_VALUE, NEUTRAL_RX_VALUE)
 	println("PWM configured for servos.")
+
+	// ESC init right away to avoid leaving some esc's in a bad state
+	// Reset retries for the next component
+	setLED(1) // R for esc init
+	retries = 0
+escInit:
+	if USE_DSHOT {
+		escPin = PWM_CH3_PIN
+		escPin.Configure(machine.PinConfig{Mode: machine.PinOutput}) // error checking is apparently not needed for PinConfig
+		setESC(MIN_PULSE_WIDTH_US)
+		println("DShot configured for ESC.")
+	} else {
+		escPWMConfig := machine.PWMConfig{
+			Period: machine.GHz * 1 / ESC_PWM_FREQUENCY,
+		}
+		if err = pwm1.Configure(escPWMConfig); err != nil {
+			setLED(7) // RGB on esc init error
+			retries++
+			if retries < 5 {
+				time.Sleep(100 * time.Millisecond)
+				goto escInit
+			}
+			// Fallback or panic if max retries exceeded
+			println("CRITICAL: Servo PWM Channel 5 Init Failed")
+			return
+		}
+		escPeriodNs = escPWMConfig.Period
+		escCh, err = pwm1.Channel(PWM_CH3_PIN)
+		if err != nil {
+			setLED(7) // RGB on esc pwm init error
+			retries++
+			if retries < 5 {
+				time.Sleep(100 * time.Millisecond)
+				goto escInit
+			}
+			// Fallback or panic if max retries exceeded
+			println("CRITICAL: ESC PWM Init Failed")
+			return
+		}
+		// need to add way to calibrate pwm esc's
+		setESC(MIN_PULSE_WIDTH_US)
+		println("PWM configured for ESC.")
+	}
 
 	i2c.Configure(machine.I2CConfig{
 		Frequency: 400 * machine.KHz,
